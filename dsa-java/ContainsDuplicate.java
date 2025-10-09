b7267014327b52419ca2bfeb84836088c89f486a
syed-wassay <syedwasay6300@gmail.com>
2025-10-10 04:47:24 +0500

    initial commit

---
+++ dsa-java/ContainsDuplicate.java
@@ -0,0 +1,12 @@
+class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,2,3,4};
+        boolean duplicate = hasDuplicate(nums);
+        System.out.println(duplicate);
+    }
+
+    public static boolean hasDuplicate(int[] nums) {
+        return false;
+    }
+}
