b2ea500f14457cf1a003ae1cb857e819106e3939
syed-wassay <syedwasay6300@gmail.com>
2025-10-11 01:48:14 +0500

    optimized solution

--- dsa-java/ContainsDuplicate.java
+++ dsa-java/ContainsDuplicate.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class ContainsDuplicate {
 
     public static void main(String[] args) {
@@ -13,16 +16,40 @@ class ContainsDuplicate {
             }
         }
 
-        boolean duplicate = hasDuplicate(nums);
+        boolean duplicate = hasDuplicateBruteForce(nums);
         System.out.println(duplicate);
+
+        boolean isDuplicate = hasDuplicateOptimized(nums);
+        System.out.println(isDuplicate);
     }
 
-    public static boolean hasDuplicate(int[] nums) {
+    public static boolean hasDuplicateBruteForce(int[] nums) {
         for (int i=0; i < nums.length -1; i++) {
             for (int j=i+1; j < nums.length; j++) {
                 if (nums[i] == nums[j]) {
                     return true;
                 }
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean hasDuplicateOptimized(int[] nums) {
+        Map<Integer, Integer> itemCounts = new HashMap<>(); // hashMap to store individual nums uniquely as keys and maintain the times they have occured in their values
+        
+        for (int num : nums) {
+            if (itemCounts.containsKey(num)) {
+                itemCounts.replace(num, itemCounts.get(num) + 1); // if key already exists increase it's count
+            }
+            else {
+                itemCounts.put(num, 1); // if key does not exist then assign it value 1
+            }
+        }
+        
+        for (int key : itemCounts.keySet()) {
+            if (itemCounts.get(key) > 1) {     // iterate over each key in hashMap and find if any key has a value greater than 1, if it does return true else return false after exiting the loop
+                return true;
             }
         }
 
