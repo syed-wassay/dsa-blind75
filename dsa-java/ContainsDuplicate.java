8806e3195baa524c2ce43297ecdacfb998aa80f6
syed-wassay <syedwasay6300@gmail.com>
2025-10-10 21:42:22 +0500

    brute force approach

--- dsa-java/ContainsDuplicate.java
+++ dsa-java/ContainsDuplicate.java
@@ -1,12 +1,31 @@
 class ContainsDuplicate {
 
     public static void main(String[] args) {
-        int[] nums = {1,3,2,3,4};
+        // int[] nums = {1,3,2,3,4};
+        // boolean duplicate = hasDuplicate(nums);
+        // System.out.println(duplicate);
+        int[] nums = new int[args.length]; // set Array size to the args length
+        for (int i=0; i < args.length; i++) {
+            try {
+                nums[i] = Integer.parseInt(args[i]); // try parsing string arg at index i to an int
+            } catch (NumberFormatException e) {
+                System.out.println("could not parse " + args[i] + " to number");
+            }
+        }
+
         boolean duplicate = hasDuplicate(nums);
         System.out.println(duplicate);
     }
 
     public static boolean hasDuplicate(int[] nums) {
+        for (int i=0; i < nums.length -1; i++) {
+            for (int j=i+1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 }
